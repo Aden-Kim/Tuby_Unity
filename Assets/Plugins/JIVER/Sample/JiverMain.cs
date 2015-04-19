34d139ba650c4f229590ee05619a0cc71f8c6a39
gunloves <gunloves@nate.com>
2015-04-19 11:29:29 +0900

    asdf

--- Assets/Plugins/JIVER/Sample/JiverMain.cs
+++ Assets/Plugins/JIVER/Sample/JiverMain.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class JiverMain : MonoBehaviour {
 	void Start () {
-		string appId = "A7A2672C-AD11-11E4-8DAA-0A18B21C2D82";
+//		App ID: 1C0C2894-E73D-4711-B9A0-A55C2C4DEBF6
+//			API Token: 95a9676a5e8d1dd421aad0a4e3d6c203752362e4
+
+		string appId = "1C0C2894-E73D-4711-B9A0-A55C2C4DEBF6";
 		string userId = SystemInfo.deviceUniqueIdentifier;
 		string userName = "Unity-" + userId.Substring (0, 5);
 		string channelUrl = "jia_test.Unity3d";
@@ -13,6 +16,7 @@ public class JiverMain : MonoBehaviour {
 		Jiver.Login (userId, userName);
 		Jiver.Join (channelUrl);
 		Jiver.Connect ();
+
 	}
 	
 	void Update () {
